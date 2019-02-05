56c482a200463cc799223d98e842b72ac4f391cf
Gene Kogan <kogan.gene@gmail.com>
2019-02-04 21:32:47 -0500

    runway model

---
+++ model.py
@@ -0,0 +1,37 @@
+import pickle
+import numpy as np
+import tensorflow as tf
+import PIL.Image
+import random
+from runway import RunwayModel
+
+
+pgan = RunwayModel()
+
+
+@pgan.setup
+def setup(alpha=0.5):
+    global Gs
+    tf.InteractiveSession()
+    batch_size = 8
+    model = 'network-final.pkl'
+    print("open model %s" % model)
+    with open(model, 'rb') as file:
+        G, D, Gs = pickle.load(file)
+    return Gs
+
+
+@pgan.command('convert', inputs={'image': 'image'}, outputs={'output': 'image'})
+def convert(Gs, inp):
+    latents = np.random.RandomState(1000).randn(1000, *Gs.input_shapes[0][1:]) # 1000 random latents
+    latents = latents[[477]]
+    labels = np.zeros([latents.shape[0]] + Gs.input_shapes[1][1:])
+    images = Gs.run(latents, labels)
+    images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8) # [-1,1] => [0,255]
+    images = images.transpose(0, 2, 3, 1) # NCHW => NHWC
+    output = np.clip(images[0], 0, 255).astype(np.uint8)
+    return dict(output=output)
+
+
+if __name__ == '__main__':
+    pgan.run()
